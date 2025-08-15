7ecb804bfa3c92b37b92047b57ddda9745d34d6b
alifoo <alissonayres.trb@gmail.com>
2025-08-15 16:02:46 -0300

    fix

--- resources/merge_sort_ref.py
+++ resources/merge_sort_ref.py
@@ -2,8 +2,8 @@ def merge_sort(data):
     if len(data) < 2:
         return data
     mid = len(data) // 2
-    left = merge_sort(data[mid:])
-    right = merge_sort(data[:mid])
+    left = merge_sort(data[:mid])
+    right = merge_sort(data[mid:])
 
     return merge(left, right)
 
