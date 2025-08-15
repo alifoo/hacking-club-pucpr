e60660b05ed72ea732ba63ea6c2670396e926ac5
alifoo <alissonayres.trb@gmail.com>
2025-08-15 03:41:22 -0300

    merge sort implementation

---
+++ resources/merge_sort_ref.py
@@ -0,0 +1,29 @@
+def merge_sort(data):
+    if len(data) < 2:
+        return data
+    mid = len(data) // 2
+    left = merge_sort(data[mid:])
+    right = merge_sort(data[:mid])
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    i = j = 0
+    final = []
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            final.append(left[i])
+            i += 1
+        else:
+            final.append(right[j])
+            j += 1
+
+    final.extend(left[i:])
+    final.extend(right[j:])
+
+    return final
+
+
+print(merge_sort((50, 23, 56, 10, 30, 88, 79)))
